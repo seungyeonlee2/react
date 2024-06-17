247fae0f2a604e0b0f9fe26703bfe2075a4ea5d4
seungyeonlee2 <tmdduslove@naver.com>
2024-06-17 12:25:45 +0900

    useRef

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './App.css';
 import AddUsers from './components/Users/AddUsers';
 import UserList from './components/Users/UserList';
@@ -10,7 +10,13 @@ const App = () => {
   // 회원들이 저장될 배열
   const [userList, setUserList] = useState([]);
 
+  const count = useRef(1);
+  console.log('count: ', count);
+
   const addUserHandler = user => {
+
+    count.current++;
+    console.log('count.current: ', count.current);
 
     console.log(user);
     setUserList(prev => [
