0e89587b79cf28f7682cf1181edeb0012d26de21
seungyeonlee2 <tmdduslove@naver.com>
2024-06-18 09:49:10 +0900

    login시 발생하는 사이드이펙트

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
 import React, { useRef, useState } from 'react';
 import './App.css';
-import AddUsers from './components/Users/AddUsers';
-import UserList from './components/Users/UserList';
-import ErrorModal from './components/UI/Modal/ErrorModal';
+import MainHeader from './components/SideEffect/MainHeader';
+import Home from './components/SideEffect/Home';
+import Login from './components/SideEffect/Login';
 
 
 const App = () => {
+  
+  // 현재 로그인 상태를 체크하는 변수
+  const [isLoggedIn, setIsloggedIn] = useState(false);
 
-  // 회원들이 저장될 배열
-  const [userList, setUserList] = useState([]);
 
-  let cnt = 1;
+  console.log('로그인 검사 수행!');
+  // localStorage에서 login-flag를 꺼냄
+  const storedLoginFlag = localStorage.getItem('login-flag');
+  // 로그인 검사를 초기에 수행
+  if(storedLoginFlag==='1'){
+    // 상태변수가 setter로 변경되면
+    // 리액트는 변경감지 후 바로 리렌더링을 수행함
+    setIsloggedIn(true);
+  }
 
-  const count = useRef(1);
-  console.log('count: ', count);
+  // 서버 통신은 중앙집중 관리가 중요함
+  const loginHandler = (email, password) => {
 
-  const addUserHandler = user => {
+    // 로그인의 증거로 클라이언트에 1이라는 숫자를 기록
+    localStorage.setItem('login-flag', '1');
+    setIsloggedIn(true);
 
-    count.current++;
-    console.log('count.current: ', count.current);
-
-    console.log(user);
-    setUserList(prev => [
-      ...prev, 
-      {
-        ...user, 
-        id: Math.random().toString()
-      }
-    ]);
   };
 
   return (
     <>
-      <AddUsers onAddUser={addUserHandler} />
-      <UserList users={userList} />
+      <MainHeader />
+      <main>
+        {isLoggedIn && <Home />}
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+      </main>
     </>
   );
 };
