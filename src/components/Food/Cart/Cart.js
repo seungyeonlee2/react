6d3f54d497b5d30980c2b44ebfd27c82ad62cd56
seungyeonlee2 <tmdduslove@naver.com>
2024-06-24 10:42:32 +0900

    # 장바구니 + 버튼 처리, 장바구니 중복 상품 제거

---
+++ src/components/Food/Cart/Cart.js
@@ -0,0 +1,63 @@
+import React, { useContext } from 'react';
+import styles from './Cart.module.scss';
+import CartModal from './CartModal';
+import CartContext from '../../../store/cart-context';
+import CartItem from './CartItem';
+
+// const DUMMY_CART = [
+//   {
+//     id: 'c1',
+//     name: '스시',
+//     amount: 2,
+//     price: 46000,
+//   },
+//   {
+//     id: 'c2',
+//     name: '띠드버거',
+//     amount: 1,
+//     price: 12000,
+//   },
+// ];
+const Cart = ({ onClose }) => {
+
+  const { cartItems, totalPrice } = useContext(CartContext);
+
+  console.log('cart: ', cartItems);
+
+  const {
+    'cart-items': cartItemStyle,
+    total,
+    actions,
+    'button--alt': btnAlt,
+    button,
+  } = styles;
+
+  return (
+    <CartModal onClose={onClose}>
+      {/* 주문 내역 */}
+      <ul className={cartItemStyle}>
+        {cartItems.map((cartItem) => (
+          <CartItem 
+            key={cartItem.id} 
+            cart={cartItem}
+          />
+        ))}
+      </ul>
+      <div className={total}>
+        <span>주문 총액</span>
+        <span>{new Intl.NumberFormat('ko-KR').format(totalPrice)}원</span>
+      </div>
+      <div className={actions}>
+        <button
+          className={btnAlt}
+          onClick={onClose}
+        >
+          닫기
+        </button>
+        <button className={button}>주문</button>
+      </div>
+    </CartModal>
+  );
+};
+
+export default Cart;
