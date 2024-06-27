0014aae48fb31a21c07b286467a42603e115c9a9
seungyeonlee2 <tmdduslove@naver.com>
2024-06-27 09:25:52 +0900

    Revert "# 장바구니 + 버튼 처리, 장바구니 중복 상품 제거"
    This reverts commit 6d3f54d497b5d30980c2b44ebfd27c82ad62cd56.

--- src/components/SideEffect/Home.js
+++ src/components/SideEffect/Home.js
@@ -6,17 +6,14 @@ import Button from '../UI/Button';
 import AuthContext from '../../store/auth-context';
 
 const Home = () => {
-
-  const { onLogout } = useContext(AuthContext);
-  // console.log('ctx: ', ctx);
-
+  
+  const {onLogout} = useContext(AuthContext);
   return (
     <Card className={styles.home}>
       <h1>Welcome back!</h1>
       <Button onClick={onLogout}>Logout</Button>
     </Card>
   );
-
 };
 
 export default Home;
